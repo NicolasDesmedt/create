84a0a6bb8e525ac5ffd81d2a21d75be64d94d9f1
NicolasDesmedt <nicolas@conveo.ai>
2024-11-14 18:43:44 +0100

    feat: update + modernize all packages

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
