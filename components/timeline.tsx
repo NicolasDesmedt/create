7c5aa7e643c7e3bcf37a76f346cc5eaf7079311a
NicolasDesmedt <nicolas@conveo.ai>
2024-11-18 21:59:42 +0100

    feat: timeline bgs per period

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -8,11 +8,10 @@ import {
   useTransform,
 } from 'framer-motion';
 import { ChevronDown } from 'lucide-react';
-import { RefObject, useEffect, useRef, useState } from 'react';
+import { RefObject, useEffect, useMemo, useRef, useState } from 'react';
 
 import ImageGallery from './image-gallery';
 
-// Add interfaces for the data structure
 interface TimelineEvent {
   date: string;
   title: string;
@@ -21,29 +20,55 @@ interface TimelineEvent {
 
 interface TimelinePeriod {
   year: number;
-  location: keyof typeof cityColors;
+  location: keyof typeof cityBackgrounds;
   events: TimelineEvent[];
 }
 
 interface TimelineEventProps {
   event: TimelineEvent;
   isEven: boolean;
-  location: keyof typeof cityColors;
+  location: keyof typeof cityBackgrounds;
 }
 
 interface ParallaxSectionProps {
   children: React.ReactNode;
 }
 
-// Update cityColors to be a const assertion for better type inference
-const cityColors = {
-  Belgium: 'from-amber-100 to-amber-200',
-  Lisbon: 'from-sky-100 to-sky-200',
-  'New York City': 'from-slate-100 to-slate-200',
-  'San Francisco': 'from-rose-100 to-rose-200',
+const cityBackgrounds = {
+  Belgium: 'antwerp-skyline',
+  Lisbon: 'lx-skyline',
+  'New York City': 'nyc-skyline',
+  'San Francisco': 'sf-skyline',
 } as const;
 
-// Update component definitions with types
+// Update cityColors to be a const assertion for better type inference
+const cityColors = {
+  Belgium: 'from-black via-yellow-500 to-red-500', // Black, yellow, and red to represent the Belgian flag
+  Lisbon: 'from-blue-500 via-teal-400 to-orange-300', // Ocean blue to vibrant terracotta, evoking Lisbon's coastal vibe
+  'New York City': 'from-gray-700 via-slate-500 to-blue-600', // Urban grays and blues to capture NYC's skyscrapers and sky
+  'San Francisco': 'from-cyan-200 via-gray-300 to-orange-400', // Soft foggy cyan to sunset orange, echoing SF’s iconic sunsets
+} as const;
+
+const createPath = (height: number) => {
+  const segments = Math.floor(height / 600); // One segment every 600px
+  let path = 'M80,-10 '; // Start at center
+
+  for (let i = 0; i < segments; i++) {
+    const yStart = i * 600;
+    const yEnd = yStart + 600;
+
+    if (i % 2 === 0) {
+      // Curve to the left, with control points to create smooth transitions
+      path += `C30,${yStart + 200} 30,${yEnd - 200} 80,${yEnd} `;
+    } else {
+      // Curve to the right, with control points to create smooth transitions
+      path += `C130,${yStart + 200} 130,${yEnd - 200} 80,${yEnd} `;
+    }
+  }
+
+  return path;
+};
+
 const TimelineEvent: React.FC<TimelineEventProps> = ({ event, isEven }) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(ref as RefObject<Element>, {
@@ -68,9 +93,7 @@ const TimelineEvent: React.FC<TimelineEventProps> = ({ event, isEven }) => {
         ref={ref}
         initial='hidden'
         animate={controls}
-        style={{
-          width: 'calc(50% - 2.5rem)',
-        }}
+        style={{ width: 'calc(50% - 2.5rem)' }}
         variants={{
           hidden: { opacity: 0, x: isEven ? 50 : -50 },
           visible: {
@@ -80,9 +103,7 @@ const TimelineEvent: React.FC<TimelineEventProps> = ({ event, isEven }) => {
           },
         }}
       >
-        <div
-          className={`transform rounded-lg bg-white/80 p-6 shadow-lg backdrop-blur-sm transition-all duration-300 hover:rotate-1 hover:scale-105`}
-        >
+        <div className='relative transform rounded-lg bg-white/80 p-6 shadow-lg backdrop-blur-sm transition-all duration-300 hover:rotate-1 hover:scale-105'>
           <div className='mb-2 flex items-center'>
             <div className='mr-2 h-3 w-3 rounded-full bg-current'></div>
             <h4 className='font-mono text-lg font-semibold'>{event.date}</h4>
@@ -91,14 +112,12 @@ const TimelineEvent: React.FC<TimelineEventProps> = ({ event, isEven }) => {
           {event.content && (
             <div className='mt-2 rounded bg-white/50 p-4'>{event.content}</div>
           )}
+          <div
+            className={`absolute top-6 ${
+              isEven ? '-left-4 -translate-x-1/2' : '-right-4 translate-x-1/2'
+            } h-4 w-4 rotate-45 transform bg-white/80`}
+          ></div>
         </div>
-        <div
-          className={`absolute top-6 ${
-            isEven ? 'left-0 md:-left-4' : 'right-0 md:-right-4'
-          } h-4 w-4 transform ${
-            isEven ? '-translate-x-1/2' : 'translate-x-1/2'
-          } rotate-45 bg-white/80`}
-        ></div>
       </motion.div>
     </div>
   );
@@ -121,21 +140,16 @@ const ParallaxSection: React.FC<ParallaxSectionProps> = ({ children }) => {
   );
 };
 
-const estimatedCardHeight = 300;
+const estimatedCardHeight = 280;
 const verticalSpacing = 150;
 
-// Add a helper function to calculate the total height needed for a period
 const calculatePeriodHeight = (period: TimelinePeriod) => {
   return period.events.length * estimatedCardHeight + verticalSpacing;
 };
 
-// Update the main Timeline component
 const Timeline: React.FC = () => {
   const [isNavVisible, setIsNavVisible] = useState(false);
   const timelineRef = useRef<HTMLDivElement>(null);
-  const [currentLocation, setCurrentLocation] = useState<
-    keyof typeof cityColors
-  >(timelineData[0].location);
 
   const scrollToYear = (year: number) => {
     const yearElement = document.getElementById(`year-${year}`);
@@ -144,52 +158,21 @@ const Timeline: React.FC = () => {
     }
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY + window.innerHeight / 2;
-      const sections =
-        document.querySelectorAll<HTMLElement>('[data-location]');
-
-      for (let i = sections.length - 1; i >= 0; i--) {
-        const section = sections[i];
-        if (section.offsetTop <= scrollPosition) {
-          if (
-            section.dataset.location &&
-            section.dataset.location in cityColors
-          ) {
-            setCurrentLocation(
-              section.dataset.location as keyof typeof cityColors
-            );
-          }
-          break;
-        }
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  useEffect(() => {
-    const timelineHeight = timelineRef.current?.clientHeight ?? 1200;
-    setSvgHeight(timelineHeight);
-  }, [timelineRef]);
-
-  const [svgHeight, setSvgHeight] = useState(5000);
-
-  const totalHeight = timelineData.reduce(
-    (total, period) => total + calculatePeriodHeight(period),
-    0
+  const totalHeight = useMemo(
+    () =>
+      timelineData.reduce(
+        (total, period) => total + calculatePeriodHeight(period),
+        0
+      ),
+    [timelineData]
   );
 
+  const svgPath = useMemo(() => createPath(totalHeight), [totalHeight]);
+
   return (
-    <div
-      className={`relative min-h-screen transition-colors duration-500 ${cityColors[currentLocation]}`}
-    >
-      <div className='container mx-auto overflow-hidden px-6 py-10'>
-        <h2 className='mb-12 text-center font-serif text-5xl font-bold italic'>
+    <div className='relative min-h-screen bg-white'>
+      <div className='container relative z-20 mx-auto overflow-hidden px-6 py-10'>
+        <h2 className='mb-24 text-center font-serif text-5xl font-bold italic'>
           My Journey
         </h2>
         <div
@@ -197,63 +180,100 @@ const Timeline: React.FC = () => {
           ref={timelineRef}
           style={{ height: `${totalHeight}px` }}
         >
-          <svg
-            className='absolute left-1/2 w-40 -translate-x-1/2 transform'
-            viewBox={`0 0 100 ${svgHeight}`}
-            preserveAspectRatio='none'
-          >
-            <motion.path
-              d='M50,0 C20,200 80,400 40,600 C10,750 90,900 50,1200 10,1350 90,1500 50,1800 10,1950 90,2100 50,2400 10,2550 90,2700 50,3000'
-              stroke='currentColor'
-              strokeWidth='40'
-              fill='none'
-              className='text-gray-900/10'
-              initial={{ pathLength: 0 }}
-              animate={{ pathLength: 1 }}
-              transition={{ duration: 3, ease: 'easeInOut' }}
-            />
+          <svg className='absolute' preserveAspectRatio='none'>
+            <defs>
+              <mask id='pathMask'>
+                <motion.path
+                  d={svgPath}
+                  stroke='white'
+                  strokeWidth='50'
+                  fill='none'
+                  initial={{ pathLength: 0 }}
+                  animate={{ pathLength: 1 }}
+                  transition={{ duration: 3, ease: 'easeInOut' }}
+                />
+              </mask>
+            </defs>
           </svg>
+          <div
+            className={`absolute left-1/2 h-full w-40 -translate-x-1/2 transform bg-gradient-to-r`}
+            style={{ WebkitMask: 'url(#pathMask)', mask: 'url(#pathMask)' }}
+          >
+            {timelineData.map((period, periodIndex) => {
+              const periodHeight = calculatePeriodHeight(period);
 
-          {timelineData.map((period, periodIndex) => {
-            // Calculate the total height of all previous periods
-            const previousPeriodsHeight = timelineData
-              .slice(0, periodIndex)
-              .reduce((total, p) => total + calculatePeriodHeight(p), 0);
-
-            return (
-              <ParallaxSection key={`${period.year}-${period.location}`}>
+              return (
                 <div
-                  className='mb-16'
-                  data-location={period.location}
+                  key={`${period.year}-${period.location}-${periodIndex}`}
+                  className={`absolute w-full bg-gradient-to-r ${cityColors[period.location]}`}
                   style={{
-                    marginTop: periodIndex === 0 ? 0 : `${verticalSpacing}px`,
-                    position: 'absolute',
-                    top: `${previousPeriodsHeight}px`,
-                    width: '100%',
+                    height: `${periodHeight}px`,
+                    top: `${PERIOD_POSITIONS[periodIndex]}px`,
                   }}
-                >
-                  <div
-                    id={`year-${period.year}`}
-                    className='mb-8 flex items-center justify-center'
+                />
+              );
+            })}
+          </div>
+          {/* <CldImage
+                width='1920'
+                height={totalHeight}
+                src={cityBackgrounds[currentLocation]}
+                sizes='100vw'
+                priority
+                className='absolute left-1/2 h-full w-40 -translate-x-1/2 transform object-cover'
+                alt={`${currentLocation} background`}
+                style={{ WebkitMask: 'url(#pathMask)', mask: 'url(#pathMask)' }}
+              /> */}
+          {timelineData.map((period, periodIndex) => (
+            <ParallaxSection
+              key={`${period.year}-${period.location}-${periodIndex}`}
+            >
+              <div
+                className='mb-16'
+                data-location={period.location}
+                style={{
+                  position: 'absolute',
+                  top: `${PERIOD_POSITIONS[periodIndex]}px`,
+                  width: '100%',
+                }}
+              >
+                <div className='mb-8 flex items-center justify-center'>
+                  <motion.div
+                    initial={{ opacity: 0, y: -60 }}
+                    whileInView={{
+                      opacity: 1,
+                      transition: {
+                        delay: periodIndex * 0.2,
+                        duration: 0.5,
+                      },
+                      y: -40,
+                    }}
+                    viewport={{ amount: 0.8, once: true }}
                   >
-                    <div className='z-10 flex h-16 w-16 transform items-center justify-center rounded-full border-4 border-current bg-white shadow-lg transition-all duration-300 hover:rotate-12 hover:scale-110'>
-                      <span className='font-mono text-lg font-bold'>
+                    <div
+                      className={`absolute left-1/2 z-10 -translate-x-1/2 transform bg-gradient-to-r ${cityColors[period.location]} px-4 py-2 text-center shadow-lg backdrop-blur-sm transition-all duration-300 hover:scale-105`}
+                      style={{
+                        clipPath: 'polygon(10% 0%, 100% 0%, 90% 100%, 0% 100%)',
+                        width: '150px',
+                      }}
+                    >
+                      <span className='font-mono text-lg font-bold text-white'>
                         {period.year}
                       </span>
                     </div>
-                  </div>
-                  {period.events.map((event, eventIndex) => (
-                    <TimelineEvent
-                      key={`${event.date}-${event.title}`}
-                      event={event}
-                      isEven={(periodIndex + eventIndex) % 2 === 0}
-                      location={period.location}
-                    />
-                  ))}
+                  </motion.div>
                 </div>
-              </ParallaxSection>
-            );
-          })}
+                {period.events.map((event, eventIndex) => (
+                  <TimelineEvent
+                    key={`${event.date}-${event.title}`}
+                    event={event}
+                    isEven={(periodIndex + eventIndex) % 2 === 0}
+                    location={period.location}
+                  />
+                ))}
+              </div>
+            </ParallaxSection>
+          ))}
         </div>
         <div className='absolute bottom-0 left-1/2 -translate-x-1/2 transform animate-bounce'>
           <ChevronDown className='h-8 w-8 text-gray-700' />
@@ -307,7 +327,7 @@ const timelineData: TimelinePeriod[] = [
         title: 'Podcast on my founder story with Emiliano',
       },
     ],
-    location: 'Belgium',
+    location: 'Lisbon',
     year: 2024,
   },
   {
@@ -334,7 +354,7 @@ const timelineData: TimelinePeriod[] = [
         title: 'Start of Y Combinator S24 in San Francisco',
       },
     ],
-    location: 'Lisbon',
+    location: 'San Francisco',
     year: 2024,
   },
   {
@@ -355,7 +375,7 @@ const timelineData: TimelinePeriod[] = [
         title: 'Co-founded Conveo.ai with Hendrik and Ben',
       },
     ],
-    location: 'San Francisco',
+    location: 'Lisbon',
     year: 2024,
   },
   {
@@ -407,5 +427,16 @@ const timelineData: TimelinePeriod[] = [
     year: 2020,
   },
 ];
+
+const PERIOD_POSITIONS = timelineData.reduce<Record<number, number>>(
+  (acc, period, index) => {
+    acc[index] =
+      index === 0
+        ? 0
+        : acc[index - 1] + calculatePeriodHeight(timelineData[index - 1]);
+    return acc;
+  },
+  {}
+);
 
 export default Timeline;
