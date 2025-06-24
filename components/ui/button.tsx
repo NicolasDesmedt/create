482b217c5644a4396f6f204b686e80871f4a906b
Nicolas Desmedt <nicolas@whop.com>
2025-06-24 15:43:09 -0400

    fix(button): adjust cva defaults

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -8,11 +8,11 @@ import { cn } from '@/lib/utils';
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
+    defaultVariants: {
+      size: 'default',
+      variant: 'default',
+    },
     variants: {
-      defaultVariants: {
-        size: 'default',
-        variant: 'default',
-      },
       size: {
         default: 'h-10 px-4 py-2',
         icon: 'h-10 w-10',
